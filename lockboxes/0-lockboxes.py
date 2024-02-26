2e45aacf2389cd8f7da86e0d0a9dc0c2f637df0e
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-26 20:53:59 +0100

    lockbox

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,6 +4,7 @@ if not sherache for the keys if existed and open them"""
 
 
 def canUnlockAll(boxes):
+    """ a function that works on unlocking boxes based on the number inside of them"""
     unlocked = [0]
     for box_id, box in enumerate(boxes):
         if not box:
