6dc60d49955db0d06bd0e60d8d1116fa0b733e2d
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-26 20:41:29 +0100

    lock box

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,14 +4,19 @@ if not sherache for the keys if existed and open them"""
 
 
 def canUnlockAll(boxes):
-    len(boxes)
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
-    queue = [0]
-    while queue:
-        box = queue.pop(0)
-        for key in boxes[box]:
-            if key < len(boxes) and not unlocked[key]:
-                unlocked[key] = True
-                queue.append(key)
-    return all(unlocked)
+    n = len(boxes)
+    opened = [False]*n
+    opened[0] = True
+
+    for _ in range(n):
+        change = False
+        for i in range(n):
+            if opened[i]:
+                for key in boxes[i]:
+                    if key < n and not opened[key]:
+                        opened[key] = True
+                        change = True
+        if not change:
+            break
+
+    return all(opened)
