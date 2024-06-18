b41bd0e1f7b280cb69ed0e24c1e9f647d4a5f133
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-06-18 10:15:27 +0100

    minioperation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,22 +3,23 @@
 """math module """
 import math
 
+
 def minOperations(n):
     if n < 2:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     """Check for smallest factors first"""
     while factor * factor <= n:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     """If n is still greater than 1, then it's prime"""
     if n > 1:
         operations += n
-    
+
     return operations
