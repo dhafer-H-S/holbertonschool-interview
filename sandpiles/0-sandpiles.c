88d1952a1b1f75cf76148fc229669513a7a21da1
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 03:39:56 +0100

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -27,22 +27,22 @@ void stabilize(int grid[3][3]);
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    /* Step 1: Add the two sandpiles */
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	int i, j;
+	/* Step 1: Add the two sandpiles */
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 
-    /* Step 2 & 3: Check stability, print and stabilize if needed */
-    while (!is_stable(grid1))
-    {
-        print_grid(grid1);
-        stabilize(grid1);
-    }
+	/* Step 2 & 3: Check stability, print and stabilize if needed */
+	while (!is_stable(grid1))
+	{
+		print_grid(grid1);
+		stabilize(grid1);
+	}
 }
 
 /**
@@ -52,18 +52,18 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  */
 bool is_stable(int grid[3][3])
 {
-    int i, j;
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                return false;
-            }
-        }
-    }
-    return true;
+	int i, j;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 /**
@@ -72,17 +72,17 @@ bool is_stable(int grid[3][3])
  */
 void print_grid(int grid[3][3])
 {
-    int i, j;
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j) printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	int i, j;
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j) printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -91,29 +91,29 @@ void print_grid(int grid[3][3])
  */
 void stabilize(int grid[3][3])
 {
-    int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-    int i, j;
+	int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                temp[i][j] -= 4;
-                if (i > 0) temp[i-1][j] += 1;
-                if (i < 2) temp[i+1][j] += 1;
-                if (j > 0) temp[i][j-1] += 1;
-                if (j < 2) temp[i][j+1] += 1;
-            }
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				temp[i][j] -= 4;
+				if (i > 0) temp[i-1][j] += 1;
+				if (i < 2) temp[i+1][j] += 1;
+				if (j > 0) temp[i][j-1] += 1;
+				if (j < 2) temp[i][j+1] += 1;
+			}
+		}
+	}
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid[i][j] += temp[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid[i][j] += temp[i][j];
+		}
+	}
 }
