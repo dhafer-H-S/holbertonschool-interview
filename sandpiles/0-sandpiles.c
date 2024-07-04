f9d73fb8eb3a307b3918e2f510f2b536920de886
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 03:18:18 +0100

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * print_grid - Prints the grid
+ * @grid: The grid to be printed
+ */
 void print_grid(int grid[3][3]);
+
+/**
+ * is_stable - Checks if the grid is stable
+ * @grid: The grid to be checked
+ * Return: True if stable, False otherwise
+ */
 bool is_stable(int grid[3][3]);
+
+/**
+ * stabilize - Stabilizes the grid
+ * @grid: The grid to be stabilized
+ */
 void stabilize(int grid[3][3]);
 
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles and stabilizes the result
+ * @grid1: The first sandpile
+ * @grid2: The second sandpile, to be added to the first
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
     int i, j;
     /* Step 1: Add the two sandpiles */
@@ -21,6 +41,11 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
     }
 }
 
+/**
+ * is_stable - Checks if a sandpile is stable
+ * @grid: Sandpile to check
+ * Return: True if stable, false otherwise
+ */
 bool is_stable(int grid[3][3]) {
     int i, j;
     for (i = 0; i < 3; i++) {
@@ -33,11 +58,18 @@ bool is_stable(int grid[3][3]) {
     return true;
 }
 
-void print_grid(int grid[3][3]) {
+/**
+ * print_grid - Prints a 3x3 grid
+ * @grid: Grid to print
+ */
+void print_grid(int grid[3][3])
+{
     int i, j;
     printf("=\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
             if (j) printf(" ");
             printf("%d", grid[i][j]);
         }
@@ -45,6 +77,10 @@ void print_grid(int grid[3][3]) {
     }
 }
 
+/**
+ * stabilize - Stabilizes a sandpile
+ * @grid: Sandpile to stabilize
+ */
 void stabilize(int grid[3][3]) {
     int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
     int i, j;
