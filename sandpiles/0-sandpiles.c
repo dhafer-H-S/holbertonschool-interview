0f72cbb1c4699de8e25ed9db230d41231fdae2d4
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 03:00:25 +0100

    sandpiles

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void print_grid(int grid[3][3]);
+bool is_stable(int grid[3][3]);
+void stabilize(int grid[3][3]);
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
+    // Step 1: Add the two sandpiles
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+
+    // Step 2 & 3: Check stability, print and stabilize if needed
+    while (!is_stable(grid1)) {
+        print_grid(grid1);
+        stabilize(grid1);
+    }
+}
+
+bool is_stable(int grid[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i][j] > 3) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void print_grid(int grid[3][3]) {
+    printf("=\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (j) printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void stabilize(int grid[3][3]) {
+    int temp[3][3] = {0};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i][j] > 3) {
+                temp[i][j] -= 4;
+                if (i > 0) temp[i-1][j] += 1; // Top
+                if (i < 2) temp[i+1][j] += 1; // Bottom
+                if (j > 0) temp[i][j-1] += 1; // Left
+                if (j < 2) temp[i][j+1] += 1; // Right
+            }
+        }
+    }
+    // Apply the toppling to the original grid
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            grid[i][j] += temp[i][j];
+        }
+    }
+}
---
+++ sandpiles/README.md
@@ -0,0 +1,1 @@
+holberton school perparation interview project Sandpiles
