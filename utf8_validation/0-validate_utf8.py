26d4302048e60a83b3cbc3e8fa278abad6bd3bc9
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-25 08:39:42 +0100

    utf 8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -22,7 +22,8 @@ def validUTF8(data):
         """
         Check if a byte is a continuation byte in UTF-8 encoding.
 
-        Continuation bytes in UTF-8 start with '10' in binary (0x80 to 0xBF in hexadecimal).
+        Continuation bytes in UTF-8 start with '10' in binary
+        (0x80 to 0xBF in hexadecimal).
 
         Parameters:
         byte (int): A single byte represented as an integer (0 to 255).
@@ -35,26 +36,18 @@ def validUTF8(data):
     num_bytes = 0  
 
     for byte in data:
-        if (byte & 0x80) == 0:
-            if num_bytes > 0:
+        if num_bytes == 0:
+            if (byte & 0x80) == 0x00:
+                continue
+            elif (byte & 0xE0) == 0xC0:
+                num_bytes = 1
+            elif (byte & 0xF0) == 0xE0:
+                num_bytes = 2
+            elif (byte & 0xF8) == 0xF0:
+                num_bytes = 3
+            else:
                 return False
-            num_bytes = 0
-        elif (byte & 0xE0) == 0xC0:
-            if num_bytes > 0:
-                return False
-            num_bytes = 1
-        elif (byte & 0xF0) == 0xE0:
-            if num_bytes > 0:
-                return False
-            num_bytes = 2
-        elif (byte & 0xF8) == 0xF0:
-            if num_bytes > 0:
-                return False
-            num_bytes = 3
         else:
-            return False
-
-        if num_bytes > 0:
             if not is_continuation_byte(byte):
                 return False
             num_bytes -= 1
